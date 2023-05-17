d1abf6c04581972e16fb035d5165da03b0ea045a
zhongqiang <zhongqiang@leedarson.com>
2023-05-17 16:49:57 +0800

    修改gitignore文件

--- itstack-demo-design-2-02/src/main/java/org/itstack/demo/desgin/factory/JDKInvocationHandler.java
+++ itstack-demo-design-2-02/src/main/java/org/itstack/demo/desgin/factory/JDKInvocationHandler.java
@@ -13,6 +13,7 @@ public class JDKInvocationHandler implements InvocationHandler {
         this.cacheAdapter = cacheAdapter;
     }
 
+    @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         return ICacheAdapter.class.getMethod(method.getName(), ClassLoaderUtils.getClazzByArgs(args)).invoke(cacheAdapter, args);
     }
